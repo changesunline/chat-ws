83f4df41f3c5487151a1e455f2c3026da90caef9
zhout <zhout@wyn88.com>
2020-08-27 14:03:56 +0800

    初步配置

---
+++ src/utils/utils.ts
@@ -0,0 +1,7 @@
+function reName(name: String): String {
+  return name
+}
+
+function name(params: string) {}
+
+export { reName }
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">hhhhhhhhh</div>
+</template>
+
+<script lang="ts">
+// import { reName } from '@/utils/utils.ts'
+export default {
+  name: 'ChatWs',
+  data () {
+    return {
+      list: [],
+    }
+  },
+}
+</script>
+
+<style></style>
